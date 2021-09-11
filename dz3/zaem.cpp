ec8fd1a94f3d2904775a03e17f12532496bd274f
Pavel <35690354+paveldm@users.noreply.github.com>
2021-09-11 15:46:39 +0300

    Add files via upload

---
+++ dz3/zaem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	float s, p, n;
+	cout << "¬ведите S, p, n" << endl;
+	if (cin >> s >> p >> n) {
+		float r = p / 100;
+		cout << (s * r * pow(1 + r, n)) / (12 * (pow(1 + r, n) - 1));
+	}
+	else {
+		cout << "¬ведена буква";
+	}
+	return 0;
+}
