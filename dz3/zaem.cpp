1a1a70e8f9c50bb7049ea25ab33c7861887d1fde
Pavel <35690354+paveldm@users.noreply.github.com>
2021-09-18 15:37:03 +0300

    Add files via upload

--- dz3/zaem.cpp
+++ dz3/zaem.cpp
@@ -6,7 +6,7 @@ int main() {
 	setlocale(0, "");
 	float s, p, n;
 	cout << "¬ведите S, p, n" << endl;
-	if (cin >> s >> p >> n) {
+	if ((cin >> s >> p >> n) and s > 0 and p > 0 and n > 0) {
 		float r = p / 100;
 		cout << (s * r * pow(1 + r, n)) / (12 * (pow(1 + r, n) - 1));
 	}
@@ -15,3 +15,7 @@ int main() {
 	}
 	return 0;
 }
+
+
+
+
