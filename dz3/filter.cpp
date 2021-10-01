75f0ca7b8fbba06b56859b5883e6cb91744cad61
Pavel <paveldm03@gmail.com>
2021-10-02 00:16:19 +0300

    Update filter.cpp

--- dz3/filter.cpp
+++ dz3/filter.cpp
@@ -4,21 +4,28 @@
 using namespace std;
 
 int main() {
-	setlocale(0, "");
 	string a, b;
-	int i;
-
 	getline(cin, a);
+	ofstream abc("1.txt");
+	abc << a;
+	abc.close();
 
-	ofstream fout("2.txt");
-	fout << a;
-	
-	for (i = 1; i <= a.size; i++) {
-		if a[i] =
+
+	for (int i = 1; i < a.size(); i++) {
+		char ch = a[i];
+		char m = a[i - 1];
+
+		if (47 < (int)ch and (int)ch < 58) {
+			if ((int)m == 45) {
+				b = b + a[i - 1] + a[i];
+			}
+			else {
+				b = b + a[i];
+			}
+		}
+		else if ((int)ch < 48 or (int)ch > 57) {
+			b = b + " ";
+		}
 	}
-
-	fout.close();
-
-	cout << a;
-
+	cout << b << "\n";
 }
