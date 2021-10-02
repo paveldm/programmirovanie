4d69f9f63e7080fae9718411ff130ede86fb39c1
Pavel <35690354+paveldm@users.noreply.github.com>
2021-10-02 15:27:34 +0300

    Add files via upload

--- dz3/sortirovka.cpp
+++ dz3/sortirovka.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main() {
-	string a;
+	string a, b;
 	a = "alrtjgnJWMflapIRNtkquWPQAwertb";
-
+	b = "                              ";
+	for (int i = 0; i < a.size() - 1; i++) {
+		int min = i;
+		char ch1 = a[min];
+		for (int j = i + 1; j < a.size(); j++) {
+			char ch2 = a[j];
+			if ((int)ch2 < (int)ch1) {
+				min = j;
+			}
+		}
+		if (min != i) {
+			swap(a[i], a[min]);
+		}
+	}
+	cout << a;
 }
