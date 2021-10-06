38d3f2c9197affbf09aca01059459ddfd1322eab
Pavel <paveldm03@gmail.com>
2021-10-07 00:05:41 +0300

    Update filter.cpp

--- dz3/filter.cpp
+++ dz3/filter.cpp
@@ -2,30 +2,42 @@
 #include <fstream>
 #include <string>
 using namespace std;
-// вход данные 		12 -12 3.5 -3344.5 .8 12qwerty
+
 int main() {
+	setlocale(0, "");
+
 	string a, b;
+	char pred;
+	pred = ' ';
+
+	ofstream file_in("1.txt");
+
 	getline(cin, a);
-	ofstream abc("1.txt");
-	abc << a;
-	abc.close();
+	file_in << a;
+	file_in.close();
+	ifstream file_read("1.txt");
 
-
-	for (int i = 1; i < a.size(); i++) {
-		char ch = a[i];
-		char m = a[i - 1];
-
-		if ('0' < ch and (int)ch < '9') {
-			if ((int)m == 45) {
-				b = b + a[i - 1] + a[i];
+	for (int i = 0; i < a.length(); i++) {
+		b = ' ';
+		bool flag = true;
+		if (isdigit(a[i]) and (pred == ' ' or pred == '-')) {
+			if (pred == ' ') {
+				b = ' ';
 			}
 			else {
-				b = b + a[i];
+				b = ' -';
+			}
+			while (a[i] != ' ' and i < a.length()) {
+				if (!(isdigit(a[i])) and a[i] != '.') {
+					flag = false;
+				}
+				b += a[i];
+				i++;
+			}
+			if (flag) {
+				cout << b; 
 			}
 		}
-		else if ((int)ch < 48 or (int)ch > 57) {
-			b = b + " ";
-		}
+		pred = a[i];
 	}
-	cout << b << "\n";
 }
